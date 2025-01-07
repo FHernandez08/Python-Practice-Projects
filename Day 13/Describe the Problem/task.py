162389c8c96e60f28c3693f4c5c7cb6b4d54296e
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-01-06 21:04:54 -0600

    Completion of day 13

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,10 +1,10 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
 
-my_function()
+print(my_function())
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
